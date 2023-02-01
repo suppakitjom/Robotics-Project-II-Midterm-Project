f2162ff94c79970a5721b8d535adbb0b20b15213
Jom Laomahamek <99940355+suppakitjom@users.noreply.github.com>
2023-02-01 13:42:14 +0700

    updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import face_recognition
+import face_recognition as fr
 import cv2
 import numpy as np
 import os
@@ -30,8 +30,7 @@ for name in facelist:
     if name[0] == ".":  # skip hidden files
         continue
     known_face_encodings.append(
-        face_recognition.face_encodings(
-            face_recognition.load_image_file("./known_faces/" + name))[0])
+        fr.face_encodings(fr.load_image_file("./known_faces/" + name))[0])
     # remove the file extension from the name
     name = name.split(".")[0]
     # handle duplicate names for people with more than 1 sample image
@@ -55,19 +54,17 @@ while True:
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
-        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which fr uses)
         rgb_small_frame = small_frame[:, :, ::-1]
 
         # Find all the faces and face encodings in the current frame of video
-        face_locations = face_recognition.face_locations(rgb_small_frame)
-        face_encodings = face_recognition.face_encodings(
-            rgb_small_frame, face_locations)
+        face_locations = fr.face_locations(rgb_small_frame)
+        face_encodings = fr.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(known_face_encodings,
-                                                     face_encoding)
+            matches = fr.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -76,8 +73,8 @@ while True:
             #     name = known_face_names[first_match_index]
 
             # Or instead, use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(
-                known_face_encodings, face_encoding)
+            face_distances = fr.face_distance(known_face_encodings,
+                                              face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
@@ -107,7 +104,7 @@ while True:
                     (255, 255, 255), 1)
 
     # Display the resulting image
-    cv2.imshow('Video', frame)
+    cv2.imshow('Video Feed', frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
