b526b74817cc2e7fca2bdb9bbc54941b95b29cba
suppakitjom <99940355+suppakitjom@users.noreply.github.com>
2023-02-13 19:14:30 +0700

    updated

--- cv/test_cv.py
+++ cv/test_cv.py
@@ -1,14 +1,17 @@
 import requests
+import os
 
 cv_url = 'http://localhost:5000/cv'
 cv_url_windows = 'http://172.20.10.2/5000/cv'
+cv_url_jetson = 'http://192.168.2.2:5000/cv'
 
 
 def getRecognizedPeople():
-    r = requests.get(cv_url)
+    r = requests.get(cv_url_jetson)
     return r.json()
 
 
 if __name__ == "__main__":
+    os.system('cls' if os.name == 'nt' else 'clear')
     print(getRecognizedPeople()['recognized'])
     pass
